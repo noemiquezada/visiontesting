712e8a61c660261b86741992bc2638881d2357a8
Noemi <noemi01@csu.fullerton.edu>
2016-02-14 08:05:04 -0800

    Added voice to the microphone window

--- viewtestingremote/viewtestingremote/VoiceViewController.h
+++ viewtestingremote/viewtestingremote/VoiceViewController.h
@@ -8,12 +8,21 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import <SpeechKit/SpeechKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "SKSConfiguration.h"
 
 
-@interface VoiceViewController : UIViewController
+@interface VoiceViewController : UIViewController <SKTransactionDelegate, SKAudioPlayerDelegate> {
+    SKSession* _skSession;
+    SKTransaction *_skTransaction;
+}
+
+- (void)resetTransaction;
 
 @property (strong, nonatomic) AppDelegate *appDelegate;
 @property (weak, nonatomic) IBOutlet UIButton *microphoneButton;
+@property (strong, nonatomic) NSString *languageCode;
 - (IBAction)speakNow:(id)sender;
 
 @end
--- viewtestingremote/viewtestingremote/VoiceViewController.m
+++ viewtestingremote/viewtestingremote/VoiceViewController.m
@@ -26,6 +26,59 @@
                                                object:nil];
 }
 
+-(void)viewWillAppear:(BOOL)animated {
+    _skTransaction = nil;
+    self.languageCode = @"eng-USA";
+    
+    _skSession = [[SKSession alloc] initWithURL:[NSURL URLWithString:SKSServerUrl] appToken:SKSAppKey];
+    
+    if (!_skSession) {
+        UIAlertView* alertView = [[UIAlertView alloc]initWithTitle:@"SpeechKit"
+                                                           message:@"Failed to initialize SpeechKit session."
+                                                          delegate:nil cancelButtonTitle:@"OK"
+                                                 otherButtonTitles:nil, nil];
+        [alertView show];
+    }
+    if (!_skTransaction) {
+        NSLog(@"In skTranslaction initialization");
+        // Start a TTS transaction
+        _skTransaction = [_skSession speakString:@"To record your answer, hold down the button"
+                                    withLanguage:self.languageCode
+                                        delegate:self];
+        
+    } else {
+        // Cancel the TTS transaction
+        [_skTransaction cancel];
+        
+        [self resetTransaction];
+    }
+    [self performSelector:@selector(viewDidLoad) withObject:nil afterDelay:3.0];
+}
+
+-(void) createTransaction: (NSString *)s {
+    if (!_skTransaction) {
+        NSLog(@"In skTranslaction initialization");
+        // Start a TTS transaction
+        _skTransaction = [_skSession speakString:s
+                                    withLanguage:self.languageCode
+                                        delegate:self];
+        
+    } else {
+        // Cancel the TTS transaction
+        [_skTransaction cancel];
+        
+        [self resetTransaction];
+    }
+}
+
+
+- (void)resetTransaction
+{
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        _skTransaction = nil;
+    }];
+}
+
 - (void)handleReceivedDataWithNotification:(NSNotification *)notification {
     // Get the user info dictionary that was received along with the notification.
     NSLog(@"Received a notification");
@@ -61,6 +114,50 @@
 - (IBAction)speakNow:(id)sender{
     NSLog(@"the user should speak now");
 }
+
+
+#pragma mark - SKTransactionDelegate
+
+- (void)transaction:(SKTransaction *)transaction didReceiveAudio:(SKAudio *)audio
+{
+    NSLog(@"didReceiveAudio");
+    
+    [self resetTransaction];
+}
+
+- (void)transaction:(SKTransaction *)transaction didFinishWithSuggestion:(NSString *)suggestion
+{
+    NSLog(@"didFinishWithSuggestion");
+    
+    // Notification of a successful transaction. Nothing to do here.
+}
+
+- (void)transaction:(SKTransaction *)transaction didFailWithError:(NSError *)error suggestion:(NSString *)suggestion
+{
+    NSLog(@"%@", [NSString stringWithFormat:@"didFailWithError: %@. %@", [error description], suggestion]);
+    
+    // Something went wrong. Check Configuration.mm to ensure that your settings are correct.
+    // The user could also be offline, so be sure to handle this case appropriately.
+    
+    [self resetTransaction];
+}
+
+#pragma mark - SKAudioPlayerDelegate
+
+- (void)audioPlayer:(SKAudioPlayer *)player willBeginPlaying:(SKAudio *)audio
+{
+    NSLog(@"willBeginPlaying");
+    
+    // The TTS Audio will begin playing.
+}
+
+- (void)audioPlayer:(SKAudioPlayer *)player didFinishPlaying:(SKAudio *)audio
+{
+    NSLog(@"didFinishPlaying");
+    
+    // The TTS Audio has finished playing.
+}
+
 
 /*
 #pragma mark - Navigation
