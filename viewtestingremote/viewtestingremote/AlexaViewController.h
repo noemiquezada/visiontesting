f1d4183deb35395098a9ec9ee650736f95fb6ad9
Neel Somani <neelsomani@Neels-MacBook-Pro.local>
2016-02-13 22:38:58 -0800

    Integrating Amazon Login

---
+++ viewtestingremote/viewtestingremote/AlexaViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlexaViewController.h
+//  viewtestingremote
+//
+//  Created by Neel Somani on 2/13/16.
+//  Copyright © 2016 Cache me if you can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlexaViewController : UIViewController
+
+@end
---
+++ viewtestingremote/viewtestingremote/AlexaViewController.m
@@ -0,0 +1,13 @@
+//
+//  AlexaViewController.m
+//  viewtestingremote
+//
+//  Created by Neel Somani on 2/13/16.
+//  Copyright © 2016 Cache me if you can. All rights reserved.
+//
+
+#import "AlexaViewController.h"
+
+@implementation AlexaViewController
+
+@end
